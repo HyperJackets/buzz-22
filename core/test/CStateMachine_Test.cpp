9157d887e2faf51c3801e6c997d92345c3c8d86c
Sean Liu <sliu750@gatech.edu>
2022-02-18 12:59:59 -0500

    Update CStateMachine_Test.cpp

--- core/test/CStateMachine_Test.cpp
+++ core/test/CStateMachine_Test.cpp
@@ -1,2 +1,11 @@
+//Please refer here for details and tips: https://docs.microsoft.com/en-us/visualstudio/test/writing-unit-tests-for-c-cpp?view=vs-2022
+
 #include "CStateMachine.hpp"
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace StateMachineSolution
+	TEST_CLASS(UnitTest1)
+{
+public:
+	TEST_METHOD()
+};
