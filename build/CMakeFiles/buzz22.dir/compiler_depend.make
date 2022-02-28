07f9248c1be8ff1ecdc61b71c56837b7d3106c9e
Sean Liu <sliu750@gatech.edu>
2022-02-27 19:46:37 -0500

    vscode updates

---
+++ build/CMakeFiles/buzz22.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for buzz22.
+# This may be replaced when dependencies are built.
--- core/src/main.cpp
+++ core/src/main.cpp
@@ -4,5 +4,11 @@ using namespace std;
 
 int main()
 {
-	std::cout << "Test";
+	std::cout << "Hello world. I hope that VS code works well." << std::endl;
+	for (int i = 0; i < 3; i++)
+	{
+		std::cout << "Charles Malloch is the goat" << std::endl;
+	}
+	double x = 5+3;
+	std::cout << x << std::endl;
 }
