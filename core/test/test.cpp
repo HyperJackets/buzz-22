22d1be5ab75986cd1d1d84f4363f5cde722c7fe6
Sean Liu <sliu750@gatech.edu>
2022-03-06 16:11:30 -0500

    Used assert in test.cpp

--- core/test/test.cpp
+++ core/test/test.cpp
@@ -1,13 +1,22 @@
 #include "C:\SEAN\HyperJackets\buzz-22\core\src\CStateMachine.hpp"
 #include <iostream>
+#include <cassert>
 
-void displayInfo(CStateMachine* pSM)
+void initialCheck(CStateMachine* pSM)
 {
-    std::cout << "Current velocity is " << pSM->getVelocity() << std::endl;
-    std::cout << "Current acceleration is " << pSM->getAccel() << std::endl;
-    std::cout << "Current battery temp is " << pSM->getBatteryTemp() << std::endl;
-    std::cout << "Current motor temp is " << pSM->getMotorTemp() << std::endl;
-    std::cout << "Current distance is " << pSM->getCurrentDist() << std::endl;
+    assert(pSM->getVelocity() == 0);
+    assert(pSM->getAccel() == 0);
+    assert(pSM->getBatteryTemp() == 50);
+    assert(pSM->getMotorTemp() == 50);
+    assert(pSM->getCurrentDist() == 0);
+    assert(pSM->getHealth());
+    assert(pSM->getLocalizationHealth());
+    assert(!pSM->getBreakEngagement());
+    assert(!pSM->getMotorEngagement());
+    assert(!pSM->getLocalizationEngagement());
+    assert(pSM->getBMSEngagement());
+    assert(pSM->getCommunicationsEngagement());
+    assert(pSM->getCurrState() == UNLOADED);
 }
 
 void displayStatesAndEngagements(CStateMachine* pSM)
@@ -19,6 +28,8 @@ int main(int, char**) {
     std::cout << "----------Testing State Machine----------" << std::endl;
     CStateMachine* mySM1 = new CStateMachine();
     //NOTE: Instead of printing, I should use asserts to check if conditions are what we expect.
-    displayInfo(mySM1);
+    //displayInfo(mySM1);
+    initialCheck(mySM1);
+
     std::cout << "----------Done testing----------" << std::endl;
 }
