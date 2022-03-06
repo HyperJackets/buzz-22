cb60a350c7e2a0f83aa6071185e503da81828cd0
Sean Liu <sliu750@gatech.edu>
2022-03-06 14:28:07 -0500

    edits to test

--- core/test/test.cpp
+++ core/test/test.cpp
@@ -1,7 +1,18 @@
 #include "C:\SEAN\HyperJackets\buzz-22\core\src\CStateMachine.hpp"
 #include <iostream>
 
+void displayInfo(CStateMachine* pSM)
+{
+    std::cout << "Current velocity is " << pSM->getVelocity() << std::endl;
+    std::cout << "Current acceleration is " << pSM->getAccel() << std::endl;
+    std::cout << "Current battery temp is " << pSM->getBatteryTemp() << std::endl;
+    std::cout << "Current motor temp is " << pSM->getMotorTemp() << std::endl;
+    std::cout << "Current distance is " << pSM->getCurrentDist() << std::endl;
+}
+
 int main(int, char**) {
-    std::cout << "Testing State Machine" << std::endl;
+    std::cout << "----------Testing State Machine----------" << std::endl;
     CStateMachine* mySM1 = new CStateMachine();
+    displayInfo(mySM1);
+    std::cout << "----------Done testing----------" << std::endl;
 }
