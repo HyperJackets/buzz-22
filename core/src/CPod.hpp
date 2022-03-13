91cefdaf45b77330426c14eb125b1786573d3f7f
Sean Liu <sliu750@gatech.edu>
2022-03-13 14:53:11 -0400

    Update CPod.hpp

--- core/src/CPod.hpp
+++ core/src/CPod.hpp
@@ -2,11 +2,22 @@
 #ifndef C_POD_HPP
 #define C_POD_HPP
 
+#include "CStateMachine.hpp"
+
 //This class simulates the pod, and has method skeletons that actually make the pod move and go through its various stages.
 class CPod
 {
+private:
+    CStateMachine* m_pStateMachine;
+
 public:
+    //get methods
+    CStateMachine* getStateMachine(void) {return this->m_pStateMachine;}
+
     //methods
-    
+    void load(void);
+    void launch(void);
+    void brake(void);
+    void crawl(void);
 };
 #endif
