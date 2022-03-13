4682a6448746df6c5783c000735085aae477d72c
Sean Liu <sliu750@gatech.edu>
2022-03-13 14:49:10 -0400

    started Pod class

---
+++ core/src/CPod.cpp
@@ -0,0 +1,2 @@
+#pragma once
+#include "CPod.hpp"
---
+++ core/src/CPod.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef C_POD_HPP
+#define C_POD_HPP
+
+//This class simulates the pod, and has method skeletons that actually make the pod move and go through its various stages.
+class CPod
+{
+public:
+    //methods
+    
+};
+#endif
