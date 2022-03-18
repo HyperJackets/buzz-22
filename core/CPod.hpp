e79dfc7dc32faefb68e7f2a180456d618097acae
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2022-03-18 12:38:46 -0400

    format to Google C++ style guideliens

--- core/CPod.hpp
+++ core/CPod.hpp
@@ -4,23 +4,26 @@
 
 #include "CStateMachine.hpp"
 
-//This class simulates the pod, and has method skeletons that actually make the pod move and go through its various stages.
-class CPod
-{
+// This class simulates the pod, and has method skeletons that actually make the
+// pod move and go through its various stages.
+class CPod {
 private:
-    CStateMachine* m_pStateMachine = new CStateMachine();
+  CStateMachine *m_pStateMachine = new CStateMachine();
 
 public:
-    //get methods
-    CStateMachine* getStateMachine(void) {return this->m_pStateMachine;}
+  // get methods
+  CStateMachine *getStateMachine(void) { return this->m_pStateMachine; }
 
-    //These methods control hardware that deal with batteries, motors, braking, propulsion, etc. We should get in touch with the hardware people and VD/aero about the exact implementation.
-    void load(void);
-    void launch(void);
-    void brake(void);
-    void crawl(void);
+  // These methods control hardware that deal with batteries, motors, braking,
+  // propulsion, etc. We should get in touch with the hardware people and
+  // VD/aero about the exact implementation.
+  void load(void);
+  void launch(void);
+  void brake(void);
+  void crawl(void);
 
-    //This method checks the pod's state machine's state, and calls one of the 4 methods as appropriate.
-    void step(void); 
+  // This method checks the pod's state machine's state, and calls one of the 4
+  // methods as appropriate.
+  void step(void);
 };
 #endif
